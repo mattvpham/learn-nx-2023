3b961111ca170fc06d38571c10de7672f33f3f97
Matt Pham <matt@nava.io>
2023-10-29 20:58:21 -0700

    Initial commit

---
+++ apps/learn-nx-2023/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'learn-nx-2023',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/learn-nx-2023'
+};
